9a356b164bfe8fedbf2b03a420fda8820206db1e
Divyanshu <53134822+dev-idblfs@users.noreply.github.com>
2020-05-01 17:59:12 +0530

    Update index.js

--- controller/index.js
+++ controller/index.js
@@ -5,19 +5,22 @@ var nodemailer = require('nodemailer');
 var path = require('path')
 const fs = require('fs');
 
-router.get("/", async function (req, res) {
+router.get("/", function (req, res) {
     var response = {};
     var transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: 'youremail@gmail.com',
-            pass: 'yourpassword'
+            type: 'oauth2',
+            user: 'developer.idblfs@gmail.com',
+            clientId: '488348022368-38ctso9p62pm7q1399auhg7lsl188ek9.apps.googleusercontent.com',
+            clientSecret: 'lY17MO6IItex0tgEP22WAYYK',
+            refreshToken: '1//04qxnRE04tFQnCgYIARAAGAQSNwF-L9Ircda2XPTt9W2nut9DSRjn_fGRLGdXYhBNmqI9zxyKhzkQvgPrXvffVlTAiSBkCgxneMg',
         }
     });
 
     var mailOptions = {
-        from: 'youremail@gmail.com',
-        to: 'myfriend@yahoo.com',
+        from: 'mr.divyanshu96@gmail.com',
+        to: 'mr.divyanshu96@gmail.com',
         subject: 'Sending Email using Node.js',
         text: 'That was easy!'
     };
