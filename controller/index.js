0909977d4db4dd37e7856261b08f0836ed36a941
Divyanshu <53134822+dev-idblfs@users.noreply.github.com>
2020-05-03 07:55:05 +0530

    Update index.js

--- controller/index.js
+++ controller/index.js
@@ -86,56 +86,56 @@ router.get("/a", async (req, res) => {
     var to = ['anshudivv@gmail.com'];
     var subject = 'NA';
     var body_html = 'BODY';
-    var response = {};
-    try {
-        const oauth2Client = await new OAuth2(
-            "488348022368-an7viup4u2mrhdhi4885sjms98b8gvgt.apps.googleusercontent.com", // ClientID
-            "H8FmLJAPOJKasOztLaxm_mT3", // Client Secret
-            "https://developers.google.com/oauthplayground" // Redirect URL
-        );
-        await oauth2Client.setCredentials({
-            refresh_token: '1//04depwKoEeBmPCgYIARAAGAQSNwF-L9IrLJibDzTlo-1k1aJ8JevFRqEU_ZBkl5qv3XWKMWOuQ-fq6tZAGxgFnF1d9TKLry7Uid0'
-        });
-        const accessToken = await oauth2Client.getAccessToken()
+    var response = { errr : "no error"};
+    // try {
+    //     const oauth2Client = await new OAuth2(
+    //         "488348022368-an7viup4u2mrhdhi4885sjms98b8gvgt.apps.googleusercontent.com", // ClientID
+    //         "H8FmLJAPOJKasOztLaxm_mT3", // Client Secret
+    //         "https://developers.google.com/oauthplayground" // Redirect URL
+    //     );
+    //     await oauth2Client.setCredentials({
+    //         refresh_token: '1//04depwKoEeBmPCgYIARAAGAQSNwF-L9IrLJibDzTlo-1k1aJ8JevFRqEU_ZBkl5qv3XWKMWOuQ-fq6tZAGxgFnF1d9TKLry7Uid0'
+    //     });
+    //     const accessToken = await oauth2Client.getAccessToken()
 
-        var transporter = await nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            service: 'gmail',
-            port: 465,
-            secure: true,
-            auth: {
-                type: 'oauth2',
-                user: 'developer.idblfs@gmail.com',
-                clientId: '488348022368-an7viup4u2mrhdhi4885sjms98b8gvgt.apps.googleusercontent.com',
-                clientSecret: 'H8FmLJAPOJKasOztLaxm_mT3',
-                refreshToken: '1//04depwKoEeBmPCgYIARAAGAQSNwF-L9IrLJibDzTlo-1k1aJ8JevFRqEU_ZBkl5qv3XWKMWOuQ-fq6tZAGxgFnF1d9TKLry7Uid0',
-            }
-        });
+    //     var transporter = await nodemailer.createTransport({
+    //         host: 'smtp.gmail.com',
+    //         service: 'gmail',
+    //         port: 465,
+    //         secure: true,
+    //         auth: {
+    //             type: 'oauth2',
+    //             user: 'developer.idblfs@gmail.com',
+    //             clientId: '488348022368-an7viup4u2mrhdhi4885sjms98b8gvgt.apps.googleusercontent.com',
+    //             clientSecret: 'H8FmLJAPOJKasOztLaxm_mT3',
+    //             refreshToken: '1//04depwKoEeBmPCgYIARAAGAQSNwF-L9IrLJibDzTlo-1k1aJ8JevFRqEU_ZBkl5qv3XWKMWOuQ-fq6tZAGxgFnF1d9TKLry7Uid0',
+    //         }
+    //     });
 
-        var mailOptions = {
-            from: from,
-            to: to,
-            subject: subject,
-            html: body_html
-        };
+    //     var mailOptions = {
+    //         from: from,
+    //         to: to,
+    //         subject: subject,
+    //         html: body_html
+    //     };
 
-        await transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                console.log('send mail error', error);
-                response = error;
-                return res.sendStatus(304).send(error);
-            }
-            else {
-                console.log('Email sent: ' + info.response);
-                response = info;
-                return res.sendStatus(200).send(info);
-            }
-        });
-    } catch (error) {
-        console.log('catch', error);
-        response = error;
-        return res.sendStatus(304).send(error);
-    }
+    //     await transporter.sendMail(mailOptions, (error, info) => {
+    //         if (error) {
+    //             console.log('send mail error', error);
+    //             response = error;
+    //             return res.sendStatus(304).send(error);
+    //         }
+    //         else {
+    //             console.log('Email sent: ' + info.response);
+    //             response = info;
+    //             return res.sendStatus(200).send(info);
+    //         }
+    //     });
+    // } catch (error) {
+    //     console.log('catch', error);
+    //     response = error;
+    //     return res.sendStatus(304).send(error);
+    // }
     res.send(response);
 })
 
