38c9bb29bf95579586f5f7438d531f79390dc113
Divyanshu <53134822+dev-idblfs@users.noreply.github.com>
2020-05-03 06:35:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require("cors")
 const cookieParse = require('cookie-parser')
 
 // init express
-var app = express()
+var app = module.exports = express()
 
 // server static files
 app.use('/public', express.static(path.join(__dirname, 'public')))
@@ -46,4 +46,4 @@ app.use((req, res, next) => {
     res.status(404).send("srroy Wrong URl");
 })
 
-app.listen(port, () => console.log(`Example app listening at http://${global.domain}`))
+app.listen(port, () => console.log(`Example app listening at http://${port}`))
