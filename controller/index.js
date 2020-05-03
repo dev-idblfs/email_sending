99ea59e044b564918041646f9e05ad58d3694c95
Divyanshu <53134822+dev-idblfs@users.noreply.github.com>
2020-05-03 07:28:41 +0530

    Update index.js

--- controller/index.js
+++ controller/index.js
@@ -6,59 +6,77 @@ var path = require('path')
 const { google } = require("googleapis");
 const OAuth2 = google.auth.OAuth2;
 
-router.get("/", async (req, res) => {
+router.post("/", async (req, res) => {
+    var from = '';
+    var to = [];
+    var subject = '';
+    var body_html = '';
     var response = {};
-    try {
-        const oauth2Client = new OAuth2(
-            "488348022368-an7viup4u2mrhdhi4885sjms98b8gvgt.apps.googleusercontent.com", // ClientID
-            "H8FmLJAPOJKasOztLaxm_mT3", // Client Secret
-            "https://developers.google.com/oauthplayground" // Redirect URL
-        );
-
-
-        oauth2Client.setCredentials({
-            refresh_token: '1//04depwKoEeBmPCgYIARAAGAQSNwF-L9IrLJibDzTlo-1k1aJ8JevFRqEU_ZBkl5qv3XWKMWOuQ-fq6tZAGxgFnF1d9TKLry7Uid0'
-        });
-
-        const accessToken = await oauth2Client.getAccessToken()
-
-        var transporter = await nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            service: 'gmail',
-            port: 465,
-            secure: true,
-            auth: {
-                type: 'oauth2',
-                user: 'developer.idblfs@gmail.com',
-                clientId: '488348022368-an7viup4u2mrhdhi4885sjms98b8gvgt.apps.googleusercontent.com',
-                clientSecret: 'H8FmLJAPOJKasOztLaxm_mT3',
-                refreshToken: '1//04depwKoEeBmPCgYIARAAGAQSNwF-L9IrLJibDzTlo-1k1aJ8JevFRqEU_ZBkl5qv3XWKMWOuQ-fq6tZAGxgFnF1d9TKLry7Uid0',
+    if (Object.keys(req.body).length > 0) {
+        try {
+            if (req.body.from) {
+                from = req.body.from
             }
-        });
-
-        var mailOptions = {
-            from: 'mr.divyanshu96@gmail.com',
-            to: ['mr.divyanshu96@gmail.com','developer.idblfs@gmail.com'],
-            subject: 'Sending Email using Node.js',
-            html: '<h1>That was easy!<h1>'
-        };
-
-        await transporter.sendMail(mailOptions, function (error, info) {
-            if (error) {
-                console.log('send mail error', error);
-                response = error
-            } else {
-                console.log('Email sent: ' + info.response);
-                response = info
+            if (req.body.to) {
+                to = req.body.to
             }
-        });
-    } catch (error) {
-        console.log('catch', error);
-        response = error;
+            if (req.body.subject) {
+                subject = req.body.subject
+            }
+            if (req.body.body_html) {
+                body_html = req.body.body_html
+            }
+            const oauth2Client = new OAuth2(
+                "488348022368-an7viup4u2mrhdhi4885sjms98b8gvgt.apps.googleusercontent.com", // ClientID
+                "H8FmLJAPOJKasOztLaxm_mT3", // Client Secret
+                "https://developers.google.com/oauthplayground" // Redirect URL
+            );
+            oauth2Client.setCredentials({
+                refresh_token: '1//04depwKoEeBmPCgYIARAAGAQSNwF-L9IrLJibDzTlo-1k1aJ8JevFRqEU_ZBkl5qv3XWKMWOuQ-fq6tZAGxgFnF1d9TKLry7Uid0'
+            });
+            const accessToken = await oauth2Client.getAccessToken()
+
+            var transporter = nodemailer.createTransport({
+                host: 'smtp.gmail.com',
+                service: 'gmail',
+                port: 465,
+                secure: true,
+                auth: {
+                    type: 'oauth2',
+                    user: 'developer.idblfs@gmail.com',
+                    clientId: '488348022368-an7viup4u2mrhdhi4885sjms98b8gvgt.apps.googleusercontent.com',
+                    clientSecret: 'H8FmLJAPOJKasOztLaxm_mT3',
+                    refreshToken: '1//04depwKoEeBmPCgYIARAAGAQSNwF-L9IrLJibDzTlo-1k1aJ8JevFRqEU_ZBkl5qv3XWKMWOuQ-fq6tZAGxgFnF1d9TKLry7Uid0',
+                }
+            });
+
+            var mailOptions = {
+                from: from,
+                to: to,
+                subject: subject,
+                html: body_html
+            };
+
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    console.log('send mail error', error);
+                    return res.sendStatus(304).send(error);
+                }
+                else {
+                    console.log('Email sent: ' + info.response);
+                    response = info;
+                    return res.sendStatus(200).send(info);
+                }
+            });
+        } catch (error) {
+            console.log('catch', error);
+            response = error;
+            return res.sendStatus(304).send(error);
+        }
+
     }
-
-    res.send(response);
+    return res.sendStatus(400).send('400');
 })
 
-
 module.exports = router;
+
