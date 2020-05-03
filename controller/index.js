985aade1f383b597cf4c0a01cf5a9736f1596c77
Divyanshu <53134822+dev-idblfs@users.noreply.github.com>
2020-05-03 08:36:07 +0530

    Update index.js

--- controller/index.js
+++ controller/index.js
@@ -8,7 +8,7 @@ const OAuth2 = google.auth.OAuth2;
 
 
 
-router.post("/", async (req, res) => {
+router.post("/send", async (req, res) => {
     var from = '';
     var to = [];
     var subject = '';
@@ -63,85 +63,22 @@ router.post("/", async (req, res) => {
                 if (error) {
                     console.log('send mail error', error);
                     response = error;
-                    return res.sendStatus(304).send(error);
+                    res.sendStatus(304).send(response);
                 }
                 else {
                     console.log('Email sent: ' + info.response);
                     response = info;
-                    return res.sendStatus(200).send(info);
+                    res.sendStatus(200).send(response);
                 }
             });
         } catch (error) {
             console.log('catch', error);
             response = error;
-            return res.sendStatus(304).send(error);
+            res.sendStatus(304).send(response);
         }
-
     }
-    res.send(response);
 })
 
-router.get("/a", (req, res) => {
-    var from = 'ansh';
-    var to = ['anshudivv@gmail.com'];
-    var subject = 'NA';
-    var body_html = 'BODY';
-    var response = {};
-    try {
-        const oauth2Client = new OAuth2(
-            "488348022368-an7viup4u2mrhdhi4885sjms98b8gvgt.apps.googleusercontent.com", // ClientID
-            "H8FmLJAPOJKasOztLaxm_mT3", // Client Secret
-            "https://developers.google.com/oauthplayground" // Redirect URL
-        );
-        oauth2Client.setCredentials({
-            refresh_token: '1//04depwKoEeBmPCgYIARAAGAQSNwF-L9IrLJibDzTlo-1k1aJ8JevFRqEU_ZBkl5qv3XWKMWOuQ-fq6tZAGxgFnF1d9TKLry7Uid0'
-        });
-        const accessToken = oauth2Client.getAccessToken()
-
-        var transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            service: 'gmail',
-            port: 465,
-            secure: true,
-            auth: {
-                type: 'oauth2',
-                user: 'developer.idblfs@gmail.com',
-                clientId: '488348022368-an7viup4u2mrhdhi4885sjms98b8gvgt.apps.googleusercontent.com',
-                clientSecret: 'H8FmLJAPOJKasOztLaxm_mT3',
-                refreshToken: '1//04depwKoEeBmPCgYIARAAGAQSNwF-L9IrLJibDzTlo-1k1aJ8JevFRqEU_ZBkl5qv3XWKMWOuQ-fq6tZAGxgFnF1d9TKLry7Uid0',
-            }
-        });
-
-        var mailOptions = {
-            from: from,
-            to: to,
-            subject: subject,
-            html: body_html
-        };
-
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                console.log('send mail error', error);
-                response.error = error;
-                res.send(response);
-            }
-            else {
-                console.log('Email sent: ' + info.response);
-                response.info = info;
-                res.send(response);
-                console.log('res1', response);
-            }
-        })
-        console.log('res2', response);
-    } catch (error) {
-        console.log('catch', error);
-        response.catch = error;
-        console.log('res3', response);
-        res.send(response);
-        // return res.sendStatus(304).send(error);
-    }
-
-})
 
 module.exports = router;
 
