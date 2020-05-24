eceec7669c35aed90cc65dbcb92a1ca9986c6068
Divyanshu <53134822+dev-idblfs@users.noreply.github.com>
2020-05-24 23:21:05 +0530

    html code

--- controller/index.js
+++ controller/index.js
@@ -122,12 +122,24 @@ router.get("/", async (req, res) => {
             // Connection:SSL/TLS
 
         });
+        let html = `<!DOCTYPE html>
+        <html>
+        <head>
+        </head>
+        <body>
+        <p style="text-align: left;"><span style="font-family: georgia, palatino;">Hey <strong>garg2509</strong>,</span></p>
+        <p style="text-align: left;"><span style="font-family: georgia, palatino;">!<strong>Verify your email - Just one more step!</strong> </span><br /><br /><span style="font-family: georgia, palatino;">Kindly enter this "<strong>123r435</strong>" verification code to proceed further. </span></p>
+        <p style="text-align: left;"><span style="font-family: georgia, palatino;">We are on a mission of finding the best suitable option for job seekers, intern seekers &amp; freelancer and make your life simpler, more productive &amp; effective. This should be easy.</span><br /><br /><span style="font-family: georgia, palatino;">To get started, first, you need to verify the email address.&nbsp;</span></p>
+        <p style="text-align: left;"><br /><span style="font-family: georgia, palatino;">Thanks &amp; Regards,</span></p>
+        <p style="text-align: left;"><span style="font-family: georgia, palatino;">Onxcy HR&nbsp;</span></p>
+        </body>
+        </html> `;
 
         var mailOptions = {
             from: 'Onxcy HR <hr@onxcy.com>',
             to: ['anshudivvy@gmail.com', 'garg2509@gmail.com'],
             subject: 'subject',
-            html: '<h1>body_html</h1>',
+            html: html,
             sender: "anshu <hr@onxcy.com>",
             replyTo: 'Onxcy <hr@onxcy.com>',
         };
