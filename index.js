965c52648e6b8c6b825c74194a9a324584785f91
Divyanshu <developer.idblfs@gmail.com>
2020-07-06 15:38:07 +0530

    update for white list domains

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.set("view engine", "ejs");
 app.use("views", express.static(path.join(__dirname, "views")));
 
 // allow cross origin
-app.use(cors());
+// app.use(cors());
 
 // app.use(formidable());
 
@@ -33,27 +33,24 @@ global.domain = "";
 
 var port = process.env.PORT || 3001;
 
-// const whitelist = ["http://localhost:8081", "https://www.onxcy.com"];
+const whitelist = ["https://www.onxcy.com"];
 
-// const corsOptionsDelegate = (req, callback) => {
-//   var corsOptions;
-//   console.log(req.headers);
-//   if (whitelist.indexOf(req.header("Origin")) !== -1) {
-//     corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
-//   } else {
-//     corsOptions = { origin: false }; // disable CORS for this request
-//     throw { massage: "You are not authrised" };
-//   }
-//   callback(null, corsOptions); // callback expects two parameters: error and options
-// };
+const corsOptionsDelegate = (req, callback) => {
+  var corsOptions;
+  if (whitelist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+    throw { message: "You are not authrised" };
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
 
-// app.all("*", cors(corsOptionsDelegate), function (req, res, next) {
-//   next();
-// });
+app.all("*", cors(corsOptionsDelegate), function (req, res, next) {
+  next();
+});
 
 app.use((req, res, next) => {
-  // res.header("Access-Control-Allow-Origin", "*");
-  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   domain = req.hostname;
   console.log(`https://${domain}`);
   next();
